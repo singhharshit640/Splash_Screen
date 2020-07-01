016ec695341c8b43e24de8b417c998a9a98daaac
singhharshit640 <51756431+singhharshit640@users.noreply.github.com>
2020-07-01 19:50:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/sleepingpandaaa/animation/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sleepingpandaaa.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+
+    ImageView ivNetflix;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivNetflix = findViewById(R.id.ivNetflix);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        ivNetflix.startAnimation(animation1);
+
+        Toast.makeText(this, "Made by Harshit", Toast.LENGTH_LONG).show();
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },5100);
+
+    }
+}
