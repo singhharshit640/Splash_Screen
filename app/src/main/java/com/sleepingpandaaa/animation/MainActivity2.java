d3bc1acffb87c6dab013b7adb6e0b5dbfe417de9
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2020-07-02 14:56:22 +0530

    Updated

--- app/src/main/java/com/sleepingpandaaa/animation/MainActivity2.java
+++ app/src/main/java/com/sleepingpandaaa/animation/MainActivity2.java
@@ -3,6 +3,7 @@ package com.sleepingpandaaa.animation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "I updated it :)", Toast.LENGTH_SHORT).show();
     }
 }
